c28d5d7f60e346a02fa90d9565142e22303ff446
phanhkn <phanhkn@gmail.com>
2023-04-16 21:09:00 -0400

    made changes

---
+++ past projects/app.js
@@ -0,0 +1,19 @@
+/*
+assign three variables
+ensure each variable involves simple arithmetics
+create an alert saying ""You have received this message because you have been chosen to open an important vault. Here is the secret combination..."
+create alert displaying the three variables
+*/
+
+//popup when page is loaded
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination..."
+);
+
+//assign three variables with arithmetics
+const num1 = 5 + 5;
+const num2 = 100 - 60;
+const num3 = 19.5 * 2;
+//popup displaying the three numbers and successful opening
+alert(num1 + "-" + num2 + "-" + num3);
+alert("congratulations, you have opened the secret vault!");
